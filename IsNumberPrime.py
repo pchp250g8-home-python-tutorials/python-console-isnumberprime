28c7777077bd3ae98d495a8107849b0bbf729957
Alexander <pchp250g8@tutamail.com>
2026-03-28 16:16:20 +0300

    Fixed Source Code

--- IsNumberPrime.py
+++ IsNumberPrime.py
@@ -5,7 +5,7 @@ nDividers = 0
 os.system("cls")
 print("Input an integer positive number")
 ulNumber = int(input())
-print(f"The number {ulNumber} has the folowing dividers:")
+print(f"The number {ulNumber} has the folowing divisors:")
 for i in range(1, ulNumber + 1):
     if ulNumber % i == 0:
         print(i, end=" ")
